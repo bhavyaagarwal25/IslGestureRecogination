3b8946ba89eb0263264ba8a7b3b558265c784fad
Bhavya <bhavyaagarwal947@gmail.com>
2026-02-15 11:04:23 +0530

    Initial Commit

---
+++ IslGestureRecogination/IslGestureRecoginationApp.swift
@@ -0,0 +1,17 @@
+//
+//  IslGestureRecoginationApp.swift
+//  IslGestureRecogination
+//
+//  Created by Bhavya Agarwal on 15/02/26.
+//
+
+import SwiftUI
+
+@main
+struct IslGestureRecoginationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
