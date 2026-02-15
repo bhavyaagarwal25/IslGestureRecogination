0b67c803a49f682260bbe505defd2eb3cacb8d7f
Bhavya <bhavyaagarwal947@gmail.com>
2026-02-15 11:09:57 +0530

    common commit

--- IslGestureRecogination/ContentView.swift
+++ IslGestureRecogination/ContentView.swift
@@ -1,4 +1,4 @@
-//
+  //
 //  ContentView.swift
 //  IslGestureRecogination
 //
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
         }
+        .padding()
         .padding()
     }
 }
